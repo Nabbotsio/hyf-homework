f32130d0a4eaf3a36eedc78a01e379a2b4097a32
Marcellus <nabbotsio@yahoo.com>
2019-01-19 20:56:53 +0100

    Background colour change part completed

--- javascript/javascript2/week2/js2homework2.js
+++ javascript/javascript2/week2/js2homework2.js
@@ -8,10 +8,12 @@ function sayFive(){
 
 
 function threeFive(startIndex, stopIndex, threeCallback, fiveCallback ){
+    // generating the array
     let numberArray =[]; 
     for(let i=startIndex; i<=stopIndex; i++){
         numberArray.push(i)
     }
+console.log(numberArray);
 
     for (i=0; i<numberArray.length; i++){
         if (numberArray[i]%3==0){
@@ -21,9 +23,59 @@ function threeFive(startIndex, stopIndex, threeCallback, fiveCallback ){
         if (numberArray[i]%5==0) {
             fiveCallback();   
         }
-        
-    }
-    console.log(numberArray);
+    } 
 }
 
 threeFive(2,15, sayThree, sayFive);
+
+function clikMebtnAction(){
+        clikMeButton.innerHTML = "You Clicked Me";
+}
+
+//creating an input field to enter numbers
+const numInput = document.createElement("input");
+document.body.appendChild(numInput);
+numInput.type = "number"
+numInput.placeholder = "type a number"
+
+// creats the click me button
+const clikMeButton = document.createElement("button");
+document.body.appendChild(clikMeButton);
+clikMeButton.innerHTML = "Click Me";
+clikMeButton.style.backgroundColor = "red";
+clikMeButton.addEventListener("click",clikMebtnAction)
+
+
+function bGndColorChange(){
+    let numberEntered = numInput.value;//captures the number inputed
+    if(numberEntered%3==0){
+        document.body.style.backgroundColor = "blue"
+    }
+    else if (numberEntered%5==0) {
+        document.body.style.backgroundColor = "yellow"
+    }
+}
+clikMeButton.addEventListener("click", bGndColorChange);
+
+const locationBtnDiv = document.createElement("div");
+document.body.appendChild(locationBtnDiv);
+const showLocationBtn = document.createElement("button");
+locationBtnDiv.appendChild(showLocationBtn);
+showLocationBtn.innerHTML = "Show Location";
+showLocationBtn.addEventListener("click", showLocationBtnClik);
+
+const locationDisplaydiv = document.createElement("div");
+document.body.appendChild(locationDisplaydiv);
+
+function showLocationBtnClik (position){
+   
+   if(!"geolocation" in navigator){
+        locationBtnDiv.innerHTML ="Lacation unavailable";
+        alert = "Geolocation not supported"
+    }
+
+        let latitude  = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        locationDisplaydiv.innerHTML = "Latitude: " + latitude + "°" + "  Longitude: " + longitude + "°";
+
+}
