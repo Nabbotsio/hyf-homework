80e82ef0967f4e0e21e4849ddb96ce09d867e9dc
Marcellus <nabbotsio@yahoo.com>
2019-02-05 21:37:35 +0100

    updated with current location weather

--- javascript/javascript3/week1/weather_app.js
+++ javascript/javascript3/week1/weather_app.js
@@ -9,26 +9,43 @@ function getWeatherInfo (){
         alert("You have to enter a city")
     }
     else{
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=336dfd8b047bd14a503a18443213c6c9")
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=336dfd8b047bd14a503a18443213c6c9")
         .then(response => response.json())
         .then((weather)=>{
             console.log(weather);
-            console.log(weather.name);
- // printing weather details          
-            let cityNameDiv = document.querySelector(".cityName")
-            cityNameDiv.innerHTML ="Weather Information for " + weather.name;
-
-            let tempDiv = document.querySelector(".temperature")
-            tempDiv.innerHTML = "Temperature: "+ Math.round(weather.main.temp - 273) + "°" + "C";
-
-            let humidityDiv=document.querySelector(".humidity");
-            humidityDiv.innerHTML= "Humidity: "+ weather.main.humidity + "%";
-
-            let discriptionDiv=document.querySelector(".description");
-            discriptionDiv.innerHTML= "Description: "+ weather.weather[0].description;
-            let image = document.querySelector(".icon");
-            image.src ="http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
+            printWeatherDetails(weather) 
         })
-    }
-    
+    }  
+}
+
+// printing weather details
+function printWeatherDetails (weather){
+    let cityNameDiv = document.querySelector(".cityName")
+    cityNameDiv.innerHTML ="Weather Information for " + weather.name;
+
+    let tempDiv = document.querySelector(".temperature")
+    tempDiv.innerHTML = "Temperature: "+ Math.round(weather.main.temp ) + "°" + "C";
+
+    let humidityDiv=document.querySelector(".humidity");
+    humidityDiv.innerHTML= "Humidity: "+ weather.main.humidity + "%";
+
+    let discriptionDiv=document.querySelector(".description");
+    discriptionDiv.innerHTML= "Description: "+ weather.weather[0].description;
+
+    let image = document.querySelector(".icon");
+    image.src ="http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
+}
+
+//  weather at current location
+document.querySelector(".current_location_weather").addEventListener("click", locationWeather);
+
+function locationWeather(){
+    navigator.geolocation.getCurrentPosition((weatherNow)=>{
+        console.log(weatherNow);
+        fetch("https://api.openweathermap.org/data/2.5/weather?lat="+ weatherNow.coords.latitude + "&lon="+ weatherNow.coords.longitude +"&units=metric&appid=336dfd8b047bd14a503a18443213c6c9")
+            .then(response => response.json())
+            .then((weather) => {
+                printWeatherDetails(weather);
+            })    
+    })
 }
