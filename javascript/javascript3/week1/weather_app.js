dbecbae6a7c943a4eeffa9fe4420bc9074d56701
Marcellus <nabbotsio@yahoo.com>
2019-02-05 05:58:23 +0100

    updated with weather icon

--- javascript/javascript3/week1/weather_app.js
+++ javascript/javascript3/week1/weather_app.js
@@ -1,22 +1,20 @@
 
+// event listener to track when user clicks that button
 document.querySelector(".get_weather").addEventListener("click", getWeatherInfo);
 
 
 function getWeatherInfo (){
     let city = document.querySelector( ".input_City").value;
-    if  (city ===""){
+    if  (city ===""){                       //makes sure user enters a city
         alert("You have to enter a city")
     }
     else{
-        //console.log(city);
-        //alert(city);
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=336dfd8b047bd14a503a18443213c6c9")
         .then(response => response.json())
         .then((weather)=>{
-            //let cityWeatherInfo = weather.filter(()=>{})
             console.log(weather);
             console.log(weather.name);
-           
+ // printing weather details          
             let cityNameDiv = document.querySelector(".cityName")
             cityNameDiv.innerHTML ="Weather Information for " + weather.name;
 
@@ -26,9 +24,10 @@ function getWeatherInfo (){
             let humidityDiv=document.querySelector(".humidity");
             humidityDiv.innerHTML= "Humidity: "+ weather.main.humidity + "%";
 
-            let discriptionDiv=document.querySelector(".description")
+            let discriptionDiv=document.querySelector(".description");
             discriptionDiv.innerHTML= "Description: "+ weather.weather[0].description;
-
+            let image = document.querySelector(".icon");
+            image.src ="http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
         })
     }
     
