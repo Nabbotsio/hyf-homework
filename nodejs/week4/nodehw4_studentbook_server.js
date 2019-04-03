f15f848f6b9e597b36e12017555272dfa26e7d39
Marcellus <nabbotsio@yahoo.com>
2019-04-03 04:26:43 +0200

    updated

--- nodejs/week4/nodehw4_studentbook_server.js
+++ nodejs/week4/nodehw4_studentbook_server.js
@@ -33,8 +33,7 @@ router.route("/students")
         res.status(404);
         res.send("Not found ");
       }
-    }
-    else if(req.query.classId) {
+    } else if (req.query.classId) {
       const classList = hyf_students.getListByClass(req.query.classId);
       if (classList.length > 0) {
         res.status(201);
@@ -43,9 +42,7 @@ router.route("/students")
         res.status(404);
         res.send("No list found for the class ");
       }
-    }
-
-    else {
+    } else {
       res.status(202);
       res.send(JSON.stringify(hyf_students.getList()));
     };
@@ -56,106 +53,35 @@ router.route("/students")
     if (!newStudent) {
       res.status(201);
       res.send("Details added " + JSON.stringify(req.body));
-    }
-    else {
+    } else {
       res.status(401);
       res.send("Student already exist ");
     }
   })
 
-  .put( (req, res) => {
+  .put((req, res) => {
     const newStudent = hyf_students.editStudentInfo(req.body);
     if (newStudent) {
       res.status(201);
       res.send("Student details edited succusfully");
-    }else {
-     res.status(401);
-      res.send("Student does not exist ")}
-    })
+    } else {
+      res.status(401);
+      res.send("Student does not exist ")
+    }
+  })
 
-    .delete((req, res) => {
-      const newStudent = hyf_students.deleteStudent(req.body);
-      //if (!newStudent) {
-        res.status(201);
-        res.send("student details deleted  " + JSON.stringify(req.body));
-      //}
-     // else {
-       // res.status(401);
-        //res.send("Student already exist ");
-      //}
-    })
-  
-
-  
+  .delete((req, res) => {
+    const newStudent = hyf_students.deleteStudent(req.body);
+    if (newStudent) {
+      res.status(201);
+      res.send("student details succesfully deleted  " + JSON.stringify(req.body));
+    } else {
+      res.status(404);
+      res.send("Student does not exist ");
+    }
+  })
 
 
-/*
-
-app.get("/getListByClass", (req, res) => {
-  const classList = hyf_students.getListByClass(req.query.classId);
-  if (classList.length > 0) {
-    res.status(201);
-    res.send(JSON.stringify(classList));
-  }
-  else {
-    res.status(404);
-    res.send("No list found for the class ");
-  }
-});
-
-app.get("/getListByClass", (req, res) => {
-  const classList = hyf_students.getListByClass(req.query.classId);
-  if (classList.length > 0) {
-    res.status(201);
-    res.send(JSON.stringify(classList));
-  }
-  else {
-    res.status(404);
-    res.send("No list found for the class ");
-  }
-});
-
-app.get("/getStudentDetailByName", (req, res) => {
-  
-});
-
-
-app.post("/addNewStudent", (req, res) => {
-  const newStudent = hyf_students.addNewStudent(req.body);
-  if (!newStudent) {
-    res.status(201);
-    res.send("Details added " + JSON.stringify(req.body));
-  }
-  else {
-    res.status(401);
-    res.send("Student already exist ");
-  }
-});
-
-app.put("/editStudentInfo", (req, res) => {
-  const newStudent = hyf_students.editStudentInfo(req.body);
-  //if (newStudent) {
-    res.status(201);
-    res.send("Student details edited succusfully");
-  //}
-  //else {
-   // res.status(401);
-    //res.send("Students already exist ");
-  //}
-});
-
-app.delete("/deleteStudent", (req, res) => {
-  const newStudent = hyf_students.deleteStudent(req.body);
-  //if (!newStudent) {
-    res.status(201);
-    res.send("student details deleted  " + JSON.stringify(req.body));
-  //}
- // else {
-   // res.status(401);
-    //res.send("Student already exist ");
-  //}
-})
-*/
 app.use('/api', router);
 
 app.listen(port, () => console.log(`Marcel HYF app is listening on port ${port}!`));
